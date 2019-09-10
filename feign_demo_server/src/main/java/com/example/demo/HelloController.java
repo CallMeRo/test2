14a39c74b3907d5a2be1aae046a549a1a362582d
wzh <wzh521002@163.com>
2019-09-10 10:40:23 +0800

    add server

---
+++ feign_demo_server/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello/feignTest")
+    @ResponseBody
+    public String index() {
+        System.out.println("client invoke");
+        return "hello from server";
+    }
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Greetings from Spring test!";
+    }
+
+}

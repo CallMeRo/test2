f9a49a8834f96d68560e2f7c0ce0554a3c08a592
wzh <wzh521002@163.com>
2019-09-11 13:52:34 +0800

    update controller

--- feign_demo_server/src/main/java/com/example/demo/HelloController.java
+++ feign_demo_server/src/main/java/com/example/demo/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
     @RequestMapping("/test")
     @ResponseBody
     public String test() {
-        return "Greetings from Spring test!";
+        return "Greetings from Spring test v2!";
     }
 
 }
